55216664ef8b8031e3cf75fecbbd38d7cac65e60
Krzysztof Mioduszewski <krzysztof.mioduszewski@allegro.com>
2024-10-27 19:43:33 +0100

    Issued nbdev_prepare

--- nbdev_sample/_modidx.py
+++ nbdev_sample/_modidx.py
@@ -5,4 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://tauzen.github.io',
                 'git_url': 'https://github.com/tauzen/nbdev_sample',
                 'lib_path': 'nbdev_sample'},
-  'syms': {'nbdev_sample.core': {'nbdev_sample.core.foo': ('core.html#foo', 'nbdev_sample/core.py')}}}
+  'syms': { 'nbdev_sample.card': { 'nbdev_sample.card.Card': ('card.html#card', 'nbdev_sample/card.py'),
+                                   'nbdev_sample.card.Card.__init__': ('card.html#card.__init__', 'nbdev_sample/card.py'),
+                                   'nbdev_sample.card.Card.__str__': ('card.html#card.__str__', 'nbdev_sample/card.py')},
+            'nbdev_sample.core': {'nbdev_sample.core.foo': ('deck.html#foo', 'nbdev_sample/core.py')}}}
